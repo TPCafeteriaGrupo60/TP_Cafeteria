9df341df32a4d1ae984cd5c2d6c6aa86868a8d04
Franco Wagenhöfer <franwagen.500@gmail.com>
2025-11-03 11:00:42 -0300

    Actualizo archivos

--- stock.h
+++ stock.h
@@ -1,5 +1,8 @@
-#ifndef STOCK_H
-#define STOCK_H
+/*#ifndef STOCK_H_INCLUDED
+#define STOCK_H_INCLUDED
+
+#include <iostream>
+#include <cstring>
 
 class Stock
 {
@@ -15,7 +18,7 @@ private:
     bool _eliminado;
 
 public:
-    Stock(); //construcxtor
+    Stock();
     Stock(int id, const char* nombre, const char* categoria, const char* unidadMedida,
           float precioCosto, float precioVenta, int stockActual, int stockMinimo);
 
@@ -41,37 +44,60 @@ public:
     void setStockMinimo(int stockMinimo);
     void setEliminado(bool eliminado);
 
-    void mostrar();
+    void Cargar();
+    void Mostrar();
 };
 
-// Funciones para manejar el archivo
-bool guardarStock(Stock producto);
-bool guardarStock(Stock producto, int posicion);
-int buscarStock(int idProducto);
-Stock leerStock(int posicion);
-int cantidadRegistrosStock();
-bool eliminarStock(int posicion);
+#endif
+*/
 
-// Funciones principales
-void listarStocks();
-void consultarStock(int idProducto);
-void alertaStockMinimo();
-int generarIdStock();
 
-// Funciones del menú
-void agregarProducto();
-void modificarProducto();
-void eliminarProducto();
-void mostrarMenuStock();
-void menuStock();
 
-// Funciones de consultas
-void consultarPorCategoria();
-void consultarPorPrecio();
-void consultarPorNivelStock();
-void menuConsultas();
+#ifndef STOCK_H
+#define STOCK_H
 
-/////Listar productos eliminados
-/////////Listar productos en seccion de modificacion
+class Stock
+{
+private:
+    int _idProducto;
+    char _nombre[40];
+    char _categoria[20];
+    char _unidadMedida[10];
+    float _precioCosto;
+    float _precioVenta;
+    int _stockActual;
+    int _stockMinimo;
+    bool _eliminado;
+
+public:
+    Stock();
+    Stock(int id, const char* nombre, const char* categoria, const char* unidadMedida,
+          float precioCosto, float precioVenta, int stockActual, int stockMinimo);
+
+    // Getters
+    int getIdProducto();
+    const char* getNombre();
+    const char* getCategoria();
+    const char* getUnidadMedida();
+    float getPrecioCosto();
+    float getPrecioVenta();
+    int getStockActual();
+    int getStockMinimo();
+    bool getEliminado();
+
+    // Setters
+    void setIdProducto(int id);
+    void setNombre(const char* nombre);
+    void setCategoria(const char* categoria);
+    void setUnidadMedida(const char* unidadMedida);
+    void setPrecioCosto(float precio);
+    void setPrecioVenta(float precio);
+    void setStockActual(int stock);
+    void setStockMinimo(int stockMinimo);
+    void setEliminado(bool eliminado);
+
+    void Cargar();
+    void Mostrar();
+};
 
 #endif
